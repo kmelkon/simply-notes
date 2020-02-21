094d6455b6e72159123e6cdee9f55900a8724f16
Karam <kmelkon@gmail.com>
2020-02-21 17:05:52 +0100

    Login wip

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { connect } from 'react-redux';
+import firebase from 'firebase/app';
 import { compose } from 'redux';
 import { firestoreConnect } from 'react-redux-firebase';
 
-const App = ({ users }) => {
-  console.log({ users });
-  return <div>Hello Friends</div>;
+const App = ({ dispatch }) => {
+  const [loading, setLoading] = useState(false);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  async function handleSubmit(e) {
+    setLoading(true);
+    e.preventDefault();
+    try {
+      await firebase.auth().signInWithEmailAndPassword(email, password);
+      console.log('bla');
+    } catch (error) {
+      console.error(error);
+    }
+    setLoading(false);
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="email"
+        value={email}
+        onChange={event => setEmail(event.target.value)}
+      />
+      <input
+        type="password"
+        value={password}
+        onChange={event => setPassword(event.target.value)}
+      />
+      <button type="submit">log in</button>
+    </form>
+  );
 };
 
-export default compose(
-  firestoreConnect(() => ['users']),
-  connect((state, props) => {
-    return { users: state.firebase.ordered.todos };
-  })
-)(App);
+export default connect(null)(App);
